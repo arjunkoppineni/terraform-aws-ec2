09686b7b4c10fada216884ce3e6210cf9fdaea7c
KOPPINENI HARISH ARJUN <95238428+arjunkoppineni@users.noreply.github.com>
2025-05-05 12:55:43 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "my-ami"{
+    type=string
+    default = ""
+    description="Amazon Machine Image ID"
+}
+variable "my-inst-type" {
+    type = string
+    default = ""
+    description="Instance type"
+  
+}
+
